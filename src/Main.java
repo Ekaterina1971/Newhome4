79c057033e035eb42414761c250a2dcadb3d5cdf
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-08-13 20:59:23 +0300

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
         for (int a = 1; a <= 10; a ++) {
             System.out.println(a);
         }
+        // задача №2
+        for (int b = 10; b > 0; b --) {
+            System.out.println(b);
+        }
     }
 }
