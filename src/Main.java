130ad057f7beac62dd671db6da6101ad38ca3275
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-08-13 20:54:32 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача №1
+        for (int a = 1; a <= 10; a ++) {
+            System.out.println(a);
+        }
     }
 }
